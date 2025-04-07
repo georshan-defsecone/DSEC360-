206d2274bc7455153a2001a502b5ced1bb97e527
dhanish <dhanish.int@defsecone.com>
2025-04-07 14:45:23 +0530

    Add SMTP, ProxyServer route

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,8 @@
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login"; 
 import About from "./pages/About";
+import SMTP from "./pages/SMTP";
+import ProxyServer from "./pages/ProxyServer";
 
 const Router = [
     {  
@@ -14,6 +16,16 @@ const Router = [
     {
         path: "/settings/about",
         element: <About/>
+    }
+    ,
+    {
+        path: "/settings/SMTP",
+        element: <SMTP/>
+    }
+    ,
+    {
+        path: "/settings/proxyserver",
+        element: <ProxyServer/>
     }
 ]
 
