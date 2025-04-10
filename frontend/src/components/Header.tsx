7547b457eb0afaac9b607bc7dceceb0747e1a959
Nagul75 <nagulk697@gmail.com>
2025-04-10 09:02:14 +0530

    Make title bold

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 export default function Header({title}: {title: string}) {
     return (
       <header className="w-full px-6 py-10 flex justify-between items-center">
-        <div className="text-xl font-semibold">{title}</div>
+        <div className="text-2xl font-bold">{title}</div>
         <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>CN</AvatarFallback>
