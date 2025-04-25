acd3254279c6375583702f4fe141c4ab88ca52c4
vignesh-int <vigneshdefsecone@gmail.com>
2025-04-25 14:40:04 +0530

    remove comments, unnessary  imports

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,7 +8,7 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
 
 type DecodedToken = {
   username: string;
@@ -16,22 +16,6 @@ type DecodedToken = {
   is_admin: boolean;
   exp: number;
 };
-
-// export default function Header({title, children}: {title: string, children?: React.ReactNode}) {
-//     return (
-//       <header className="w-full px-6 py-10 flex justify-between items-center">
-//         <div className="text-xl font-semibold">{title}</div>
-//         <div>
-//           {children}
-//         </div>
-//         <Avatar>
-//             <AvatarImage src="https://github.com/shadcn.png" />
-//             <AvatarFallback>CN</AvatarFallback>
-//         </Avatar>
-//       </header>
-//     )
-//   }
-  
 
 export default function Header({title, children}: {title: string, children?: React.ReactNode}) {
   const [username, setUsername] = useState("");
