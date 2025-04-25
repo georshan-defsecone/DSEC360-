eb1a108d4c8aad370599d90f594144ffcbd1e1cf
vignesh-int <vigneshdefsecone@gmail.com>
2025-04-25 09:31:35 +0530

    resolve commit changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -17,23 +17,23 @@ type DecodedToken = {
   exp: number;
 };
 
-export default function Header({title, children}: {title: string, children?: React.ReactNode}) {
-    return (
-      <header className="w-full px-6 py-10 flex justify-between items-center">
-        <div className="text-xl font-semibold">{title}</div>
-        <div>
-          {children}
-        </div>
-        <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </header>
-    )
-  }
+// export default function Header({title, children}: {title: string, children?: React.ReactNode}) {
+//     return (
+//       <header className="w-full px-6 py-10 flex justify-between items-center">
+//         <div className="text-xl font-semibold">{title}</div>
+//         <div>
+//           {children}
+//         </div>
+//         <Avatar>
+//             <AvatarImage src="https://github.com/shadcn.png" />
+//             <AvatarFallback>CN</AvatarFallback>
+//         </Avatar>
+//       </header>
+//     )
+//   }
   
-=======
-export default function Header({ title }: { title: string }) {
+
+export default function Header({title, children}: {title: string, children?: React.ReactNode}) {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
 
@@ -59,6 +59,9 @@ export default function Header({ title }: { title: string }) {
   return (
     <header className="w-full px-6 py-6 flex justify-between items-center ">
       <div className="text-xl font-semibold text-gray-800">{title}</div>
+      <div>
+         {children}
+     </div>
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
@@ -104,4 +107,4 @@ export default function Header({ title }: { title: string }) {
     </header>
   );
 }
->>>>>>> main
+
