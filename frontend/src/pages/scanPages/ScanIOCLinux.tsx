46a20d81875d2c8ed25a06c4455fe77ef64cb1f0
Nagul75 <nagul697@gmail.com>
2025-05-21 11:14:09 +0530

    Fix card size and make shadow bolder

--- frontend/src/pages/scanPages/ScanIOCLinux.tsx
+++ frontend/src/pages/scanPages/ScanIOCLinux.tsx
@@ -837,9 +837,9 @@ const ScanIOCLinux = () => {
         <div className="flex-1 flex flex-col pr-8 pl-8 ml-64 pt-20">
           <Header title="Linux Compromise Assessment Scan" />
 
-          <Card className="w-[85%] mt-4 ml-4">
-            <CardContent className="w-full p-4 pl-12">
-              <div className="w-[90%] space-y-6">
+          <Card className="w-[70%] mt-4 ml-4 shadow-2xl">
+            <CardContent className="w-full p-4 px-12">
+              <div className="w-auto space-y-6">
                 <form onSubmit={handleSubmit}>
                   {renderPage()}
 
--- frontend/src/pages/scanPages/ScanIOCWindows.tsx
+++ frontend/src/pages/scanPages/ScanIOCWindows.tsx
@@ -15,15 +15,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { ChevronDown } from "lucide-react";
 
 import api from "../api";
 const ScanIOCWindows = () => {
@@ -846,9 +837,9 @@ const ScanIOCWindows = () => {
         <div className="flex-1 flex flex-col pr-8 pl-8 ml-64 pt-20">
           <Header title="Windows Compromise Assessment Scan" />
 
-          <Card className="w-[85%] mt-4 ml-4">
-            <CardContent className="w-full p-4 pl-12">
-              <div className="w-[80%] space-y-6">
+          <Card className="w-[70%] mt-4 ml-4 shadow-2xl">
+            <CardContent className="w-full p-4 px-12">
+              <div className="w-auto space-y-6">
                 <form onSubmit={handleSubmit}>
                   {renderPage()}
 
