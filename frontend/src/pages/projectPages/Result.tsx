fde613b36bd8e32674dbd35627ede550de2cfa71
Nagul75 <nagul697@gmail.com>
2025-05-22 09:14:18 +0530

    Remove grid and add msg if there are no results

--- frontend/src/pages/projectPages/Result.tsx
+++ frontend/src/pages/projectPages/Result.tsx
@@ -1,74 +1,81 @@
-import Header from "@/components/Header"
-import Sidebar from "@/components/Sidebar"
-import { useEffect, useState } from 'react';
-import api from "../api"
-import { Card, CardContent } from "@/components/ui/card"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import { useEffect, useState } from "react";
+import api from "../api";
+import { Card, CardContent } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 function ProjectScans() {
+  const [scans, setScans] = useState([]);
 
-    const [scans, setScans] = useState([]);
-    
-    useEffect(() => {
-      const fetchScans = async () => {
-        try {
-          const response = await api.get('scans/user', {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("access")}`,
-            },
-          });
-          setScans(response.data);
-        } catch (err) {
-          console.error("Error fetching scans:", err);
-        }
-      };
-      fetchScans();
-    }, []);
-    
-    
-    return (
-        <div className="flex h-screen text-black pt-18">
-            <Sidebar settings={false} scanSettings={false} homeSettings={true} />
-            <div className="flex-1 flex flex-col ml-64">
-                <Header title={"Results"}></Header>
-                <div className="p-4 overflow-auto max-h-[calc(100vh-100px)]">
-                    <div className="grid lg:grid-cols-1 gap-4">
-                        <div className="col-span-2">
-                            <Card className="mt-3 w-full">
-                                <CardContent className="p-4">
-                                    <ScrollArea className="rounded-md border">
-                                        <Table>
-                                            <TableHeader>
-                                                <TableRow>
-                                                    <TableHead className="w-[40px]"></TableHead>
-                                                    <TableHead>Scan Name</TableHead>
-                                                    <TableHead>Scan Author</TableHead>
-                                                    <TableHead>Scan Status</TableHead>
-                                                </TableRow>
-                                            </TableHeader>
-                                            <TableBody>
-                                                {scans.map((scan, i) => (
-                                                    <TableRow key={i}>
-                                                        <TableCell></TableCell>
-                                                        <TableCell className="font-medium">{scan.scan_name}</TableCell>
-                                                        <TableCell>{scan.scan_author}</TableCell>
-                                                        <TableCell>{scan.scan_status}</TableCell>
-                                                    </TableRow>
-                                                ))}
-                                                
-                                            </TableBody>
-                                        </Table>
-                                    </ScrollArea>
-                                </CardContent>
-                            </Card>
-                        </div>
-                    </div>
+  useEffect(() => {
+    const fetchScans = async () => {
+      try {
+        const response = await api.get("scans/user", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access")}`,
+          },
+        });
+        setScans(response.data);
+      } catch (err) {
+        console.error("Error fetching scans:", err);
+      }
+    };
+    fetchScans();
+  }, []);
 
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex h-screen text-black">
+      <Sidebar settings={false} scanSettings={false} homeSettings={true} />
+      <div className="flex-1 flex flex-col ml-64 pt-20">
+        <Header title={"Results"}></Header>
+        <Card className="mt-3 w-[80%] shadow-2xl ml-12">
+          <CardContent className="p-4 px-12">
+            <ScrollArea className="rounded-md border">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Scan Name</TableHead>
+                    <TableHead>Scan Author</TableHead>
+                    <TableHead>Scan Status</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {scans.length === 0 ? (
+                    <>
+                      <TableRow>
+                        <TableCell colSpan={4} className="text-center">
+                          No projects found.
+                        </TableCell>
+                      </TableRow>
+                    </>
+                  ) : (
+                    scans.map((scan, i) => (
+                      <TableRow key={i}>
+                        <TableCell className="font-medium">
+                          {scan.scan_name}
+                        </TableCell>
+                        <TableCell>{scan.scan_author}</TableCell>
+                        <TableCell>{scan.scan_status}</TableCell>
+                      </TableRow>
+                    ))
+                  )}
+                </TableBody>
+              </Table>
+            </ScrollArea>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
-export default ProjectScans
+export default ProjectScans;
