b0bad75cc4b0c0d55ff0d36c489a5e54d75aa12c
Nagul75 <nagul697@gmail.com>
2025-05-22 12:04:07 +0530

    Refactor page to show a basic message

--- frontend/src/pages/scanPages/ScanHome.tsx
+++ frontend/src/pages/scanPages/ScanHome.tsx
@@ -8,20 +8,11 @@ const ScanHome = () => {
       <div className="flex h-screen text-black pt-24">
         <Sidebar settings={false} scanSettings={true} homeSettings={false} />
         <div className="flex-1 flex flex-col pr-8 pl-8 ml-64">
-          <Header title="" />
-          <Card className="h-130">
-            <CardContent className="p-4 pl-8">
-              <h3 className="font-bold text-xl text-gray-700">
-                Defsecone Scanner
-              </h3>
-              <div className="flex flex-row">
-                <div className="flex justify-between w-60">
-                  <p className="mt-10 ml-4 text-lg font-semibold">Version:</p>
-                  <p className="mt-10 ml-4 text-lg font-semibold">10.2.0</p>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
+          <Header title={"Scan Home"} />
+          <div className="flex flex-col justify-center items-center gap-3 pb-45 h-screen">
+            <h1 className="text-3xl font-normal">Get started by <span className="text-decoration-line: underline">running a scan.</span></h1>
+            <h2 className="text-2xl font-normal">Choose a scan type from the sidebar to begin.</h2>
+          </div>
         </div>
       </div>
     </>
