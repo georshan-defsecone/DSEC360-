755552d3aa0437428fcbcacb818f1bfdd61b346b
Nagul75 <nagul697@gmail.com>
2025-05-27 09:50:56 +0530

    Style breadcrumbs

--- frontend/src/components/ui/Breadcrumbs.tsx
+++ frontend/src/components/ui/Breadcrumbs.tsx
@@ -10,15 +10,15 @@ const Breadcrumbs = ({currentPage, pages}: BreadcrumbProps) => {
         <nav className="flex" aria-label="Breadcrumb">
             <ol className="inline-flex items-center space-x-1 md:space-x-2">
                 {pages.map((page, index) => (
-                    <li key={index} className="inline-flex items-center">
+                    <li key={index} className="inline-flex items-center text-sm">
                         {index > 0 && (
                             <ChevronRight className="w-4 h-4 text-gray-400 mx-1"/>
                         )}
-                        <span className={`inline-flex items-center text-sm font-medium ${
-                            currentPage === index + 1 ? "text-black font-semibold" : "text-gray-500"
+                        <span className={`inline-flex items-center font-medium  ${
+                            currentPage === index + 1 ? "text-black font-bold" : "text-gray-400"
                         }`}>{page}</span>
                     </li>
-                ))}
+                ))} 
             </ol>
         </nav>
     )
